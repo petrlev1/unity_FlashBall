18eb60e2d58cd7812a09c40d5b8c391323dc0605
Petr-ПК\Petr <levendeevp@gmail.com>
2019-02-01 15:08:17 +0300

    Сцена, играк и два преследующих врага. При сталкновении высвечивается в консоль имя игрока

---
+++ Assets/Looker.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Looker : MonoBehaviour
+{
+	
+	
+	
+    private void OnCollisionEnter(Collision collision)
+{
+        Debug.Log ( gameObject.name );
+}
+
+}
