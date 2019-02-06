b82af6e42ade6241ab75a602be3b3d9b531d9509
Petr-ПК\Petr <levendeevp@gmail.com>
2019-02-07 01:45:30 +0300

    Добавлено управление джойстиком

--- Assets/scripts/Player.cs
+++ Assets/scripts/Player.cs
@@ -1,12 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
 
 public class Player : MonoBehaviour
 
 {
 	
 	private Rigidbody rb;
+	
+	public float Speed = 10.0f;
 	
     // Start is called before the first frame update
     void Start()
@@ -21,15 +24,24 @@ public class Player : MonoBehaviour
     {
 		
 		// Управление
+
+		// Джойстик
+		rb.AddForce(Vector3.right*CrossPlatformInputManager.GetAxis("Horizontal") * Speed );
+		rb.AddForce(Vector3.forward*CrossPlatformInputManager.GetAxis("Vertical") * Speed );
 		
-if (Input.GetKey(KeyCode.UpArrow))
+		// Клавиатура Rigidbody
+/* if (Input.GetKey(KeyCode.UpArrow))
             rb.AddForce(Vector3.forward * 20);
 if (Input.GetKey(KeyCode.DownArrow))
             rb.AddForce(Vector3.back * 20);
 if (Input.GetKey(KeyCode.LeftArrow))
             rb.AddForce(Vector3.left * 20);
 if (Input.GetKey(KeyCode.RightArrow))
-            rb.AddForce(Vector3.right * 20);
+            rb.AddForce(Vector3.right * 20); */
+		
+		//Клавиатура - без Rigidbody
+		 //transform.Translate(Vector3.forward*Input.GetAxis("Vertical")*Speed.x*Time.deltaTime);
+		 //transform.Translate(Vector3.right*Input.GetAxis("Horizontal")*Speed.y*Time.deltaTime);
 		
 		
         
